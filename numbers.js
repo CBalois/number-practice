5e4e4f6c85a07a831083834652728421b102babd
Camelle <baloiscamelle@gmail.com>
2024-09-13 21:58:05 +0800

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,20 @@
+let firstFavNum = 25;
+let secondFavNum = 22;
+
+//addition
+let sum = firstFavNum + secondFavNum;
+console.log(sum);
+//Subtraction
+const diffrence = firstFavNum - secondFavNum;
+//Multiplication
+const product = firstFavNum * secondFavNum;
+console.log (product);
+//Division
+const qoutient = firstFavNum / secondFavNum;
+console.log(qoutient);
+//Modulus
+const remainder = firstFavNum % secondFavNum;
+console.log(remainder);
+//Exponentiation(Power)
+const power = firstFavNum ** secondFavNum;
+console.log(power);
